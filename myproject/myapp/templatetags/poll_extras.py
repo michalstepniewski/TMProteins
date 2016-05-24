595149574cc5eb486c3f74da98cfb7b1f436ffd9
michalstepniewski <michalstepniewski@gmail.com>
2016-05-24 21:35:28 +0200

    added filter

--- myproject/myapp/templatetags/poll_extras.py
+++ myproject/myapp/templatetags/poll_extras.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name='format83f')
 def format83f(value): # Only one argument.
     """Converts a string into all lowercase"""
     return '%8.3f' % (value)
