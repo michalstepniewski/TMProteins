66f9783dae1ca3ab76b893e08a0c7bd99b46c4c6
michalstepniewski <michalstepniewski@gmail.com>
2016-06-24 23:12:25 +0200

    added match.py

--- myapp/models.py
+++ myapp/models.py
@@ -14,6 +14,7 @@ import math
 from django.db.models import Sum, Avg
 from GeometricalClassesModule import SetOfVectors, Vector, SetOfPoints, Point
 from django.db.models import Avg, Max, Min
+import match
 from match import rmsd
 #print 'importing Picture'
 #from fileupload.models import Picture
